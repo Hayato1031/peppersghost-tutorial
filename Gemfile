8c92b4d3aecda468a925415d1c8c72870df40899
EC2 Default User <ec2-user@ip-10-1-0-133.ap-northeast-1.compute.internal>
2023-12-13 05:26:36 +0000

    first commit

---
+++ Gemfile
@@ -0,0 +1,13 @@
+ruby '3.0.0'
+source 'https://rubygems.org'
+
+gem 'sinatra', '~> 2.1'
+gem 'sinatra-contrib', '~> 2.1'
+
+#websocket用
+gem 'puma'
+gem 'faye-websocket'
+
+#LINEbot用
+gem "dotenv"
+gem "line-bot-api"
---
+++ app.rb
@@ -0,0 +1,69 @@
+require 'bundler/setup'
+Bundler.require
+require 'sinatra/reloader' if development?
+Dotenv.load
+require 'faye/websocket'
+set :sockets, []
+
+def client
+   @client ||= Line::Bot::Client.new{|config|
+       config.channel_id = ENV["LINE_CHANNEL_ID"]
+       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+       config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+   } 
+end
+
+post '/webhook' do
+    body = request.body.read
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+       error 400 do 'Bad Request' end 
+    end
+    
+    events = client.parse_events_from(body)
+    events.each do |event|
+        if event.is_a?(Line::Bot::Event::Message)
+            if event.type === Line::Bot::Event::MessageType::Text
+                message = {
+                    type:'text',
+                    text: event.message['text']
+                }
+                client.reply_message(event['replyToken'], message)
+                
+                # WebSocketを通じて受信したメッセージを送信する
+                settings.sockets.each do |socket|
+                    socket.send(event.message['text'])
+                end
+            end
+        end
+    end
+    
+    "OK"
+end
+
+get '/websocket' do
+  if Faye::WebSocket.websocket?(request.env)
+    ws = Faye::WebSocket.new(request.env)
+
+    ws.on :open do |event|
+      settings.sockets << ws
+    end
+
+    ws.on :message do |event|
+      settings.sockets.each do |socket|
+        socket.send(event.data)
+      end
+    end
+
+    ws.on :close do |event|
+      ws = nil
+      settings.sockets.delete(ws)
+    end
+
+    ws.rack_response
+  end
+end
+
+get '/' do
+    erb :index
+end
